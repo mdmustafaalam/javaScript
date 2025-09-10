eda89c34b0ffe18b079ef1744e113bf4fbd3f48a
mdmustafaalam <mustafaalam3240@gmail.com>
2025-09-11 00:30:41 +0530

    dataTypeSummery

---
+++ dataTypeSummery.js
@@ -0,0 +1,31 @@
+// primitive 
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.30;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const userId = Symbol('123');
+
+// console.log(id === userId);
+// const bigNumber = 6516189848453119848n;
+
+
+// Reference (Non Primitive)
+// type : Array, Objects, Functions
+
+const hero = ["SpidersMan", "TonyStack", "Thanos"];
+
+let myObj = {
+    name: "Mustafa",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
