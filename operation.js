986cfbca7d44aebf042b986a9bf7e92e44812704
mdmustafaalam <mustafaalam3240@gmail.com>
2025-09-10 23:50:30 +0530

    operation all data updated

--- operation.js
+++ operation.js
@@ -28,5 +28,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+let value = 3;
+let negativeValue = -value;
+
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Mustafa";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((1 + 2) * (6 % 5));
+
+let gameCounter = 100;
+// ++gameCounter;
+gameCounter++;
+
+console.log(gameCounter);
+
+
+
