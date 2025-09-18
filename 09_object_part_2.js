ee3818f0044f47617e14133ed9597634f75ffbff
mdmustafaalam <mustafaalam3240@gmail.com>
2025-09-18 21:11:14 +0530

    Objects Part Two

--- 09_object_part_2.js
+++ 09_object_part_2.js
@@ -19,4 +19,13 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName.lastName);
+// console.log(regularUser.fullName.lastName);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {2: "a", 3: "b"};
+const obj3 = {4: "a", 5: "b"};
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+const obj4 = {...obj1, ...obj2, ...obj3};
+// console.log(obj4);
+
