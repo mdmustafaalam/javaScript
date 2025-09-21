27d1b99cde5a22aa5cf47f9c0eece8c80d94b341
mdmustafaalam <mustafaalam3240@gmail.com>
2025-09-21 22:30:45 +0530

    Funcction

---
+++ 10_function.js
@@ -0,0 +1,12 @@
+function sayMyName(){
+    console.log("M");
+    console.log("U");
+    console.log("S");
+    console.log("T");
+    console.log("A");
+    console.log("F");
+    console.log("A");
+}
+
+sayMyName //Reference
+sayMyName(); // Execution
