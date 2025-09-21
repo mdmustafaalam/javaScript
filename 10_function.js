d76594b8c5370ad0f04ad39e0980770586325c88
mdmustafaalam <mustafaalam3240@gmail.com>
2025-09-21 22:46:05 +0530

    Function

--- 10_function.js
+++ 10_function.js
@@ -15,4 +15,13 @@ function addTwoNumber(num1, num2){  // Parameters
     console.log(num1 + num2);
 }
 
-addTwoNumber(3, 5); // Arguments
+// addTwoNumber(3, 5); // Arguments
+
+function storeResult(n1, n2){
+    let result = n1 + n2;
+    return result;
+}
+
+const result = storeResult(5,8);
+
+console.log(`Result is: ${result}`);
